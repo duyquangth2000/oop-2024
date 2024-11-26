f854bfbccdb5a9eb9e373fd2aee6db941bf95a13
Your Name <you@example.com>
2024-11-26 21:48:02 +0700

    method-overloading

---
+++ OtherProjects/Lab01/HelloWorld.java
@@ -0,0 +1,8 @@
+//Example 2.1: HelloWorld.java
+//Test-printing program
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+    } // end of method main
+}
